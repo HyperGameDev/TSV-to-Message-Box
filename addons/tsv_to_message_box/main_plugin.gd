45667c72e694d983ab7dedb62d085ea461845df7
Lýz <147095077+HyperGameDev@users.noreply.github.com>
2025-04-18 22:14:42 -0500

    Improvements

--- addons/tsv_to_message_box/main_plugin.gd
+++ addons/tsv_to_message_box/main_plugin.gd
@@ -4,4 +4,5 @@ extends Control
 @onready var button_upload: Button = %Button_Upload
 @onready var file_name: Label = %Label_FileName
 @onready var vbox_tsv_table: VBoxContainer = %VBox_LoadedTSV
-@onready var vbox_tsv_preview: VBoxContainer = %VBox_TSV_to_Generate
+@onready var tsv_preview: PanelContainer = %TSV_Preview
+@onready var button_generate: Button = %Button_Generate
